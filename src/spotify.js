1c58e6e78cf6f747564f64c1bd69e537c562de6f
mikosramek <miko@mikosramek.ca>
2020-01-08 11:19:26 -0500

    Making express handle the spotify auth. The localhost port listens for a code param and then can use that for auth

--- src/spotify.js
+++ src/spotify.js
@@ -1,7 +1,5 @@
 const axios = require('axios');
-const express = require('express');
-const spotifyInterface = express();
-
+const http = require('http');
 
 const spotify = {};
 
@@ -11,13 +9,28 @@ spotify.init = (clientId, clientSecret) => {
   spotify.clientId = clientId;
   spotify.clientSecret = clientSecret;
 
-  spotifyInterface.get('/', (req, res) => {
-    res.send('Hello World!');
-  });
-  spotifyInterface.listen(3000, function () {
-    console.log('Spotify Interace listening!');
-  })
+  const express = require('express');
+  const app = express();
+  app.set('view engine', 'ejs');
+  spotify.port = 6969;
 
+  app.get('/', function (req, res) {
+    const code = req.query.code;
+    if(code) {
+      spotify.authCode = code;
+      spotify.confirmAuth();
+    }
+    res.render('index');
+  });
+
+  const url = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&scope=user-read-private%20user-read-email&redirect_uri=http://localhost:${spotify.port}/`;
+
+  app.listen(spotify.port, function () {
+    console.log(`Spotify Auth listening for callback on port ${port}!`);
+    console.log(`Go to ${url} to link your spotify account.`)
+  });
+
+  
 }
 
 spotify.getToken = () => {
@@ -26,67 +39,71 @@ spotify.getToken = () => {
     url: `https://accounts.spotify.com/authorize`,
     dataResponse: 'json',
     params: {
-      client_id:clientId,
+      client_id:spotify.clientId,
       response_type: 'code',
-      redirect_uri: 'https%3A%2F%2Fexample.com%2Fcallback',
+      redirect_uri: 'http://localhost:3000/',
       scope: 'user-read-private%20user-read-email'
     }
   }).then( (result) => {
-    // csrf_token=AQCR2scheKPnu9EYKGgLtYd3Sz18y6cZcX8RmH7z8BVZjMfNPl89_fHs-rp7Xv3fkBZXuBN5zN660S2jpQ
     console.log(result);
-    console.log(result.headers['set-cookie'][1]);
-    const authToken = result.headers['set-cookie'][1].split(';')[0].replace('csrf_token=', '');
-    console.log(authToken);
+
+    // csrf_token=AQCR2scheKPnu9EYKGgLtYd3Sz18y6cZcX8RmH7z8BVZjMfNPl89_fHs-rp7Xv3fkBZXuBN5zN660S2jpQ
+    // console.log(result);
+    // console.log(result.headers['set-cookie'][1]);
+    // const authToken = result.headers['set-cookie'][1].split(';')[0].replace('csrf_token=', '');
+    // console.log(authToken);
     //AQDp04D0_7qnzxpMfEJJ4EbiztxdUC3zpjB2BMK5lahU2spaVNTyJgSQiFE-EW88PHmeTXMKjDQtzyhPM3LJjfT9cubOfNzam7bQhpW3r_z3PPaojGF7tv7mro13upA3avnQHeXnvwRZ6oSPre8utNJpJSK0Z6SwcRrixoh7SNm0PZDAPdNyEvdXyqXLi10v6X1Y8f-RYZZzyUqK8pXkWaO0kPgYmptsyMpE00VbtHwQFic6zG0
-
-
-    // axios.post(
-    //   'https://accounts.spotify.com/api/token', new URLSearchParams ( {
-    //     grant_type: 'authorization_code',
-    //     code: authToken,
-    //     redirect_uri: 'https%3A%2F%2Fexample.com%2Fcallback'
-    //   }).toString(),
-    //   {
-    //     headers: {
-    //       'Content-Type': 'application/x-www-form-urlencoded',
-    //       'Authorization': `Basic ${clientId}:${clientSecret}`
-    //     },
-    //   }
-    // ).then( (response) => {
-    //   // console.log(response);
-    // }).catch( (error) => {
-    //   // console.log(error);
-    // })
-
-
-    // console.log(auth);
-    // var auth = 'Basic ' + new Buffer(clientId + ':' + clientSecret);
-    // axios({
-    //   method: 'POST',
-    //   url: 'https://accounts.spotify.com/api/token',
-    //   dataResponse: 'json',
-    //   params : {
-    //     grant_type: 'client_credentials',
-    //     code: authToken,
-    //   },
-    //   data: {
-        
-        
-    //     // redirect_uri: 'https%3A%2F%2Fmikosramek.ca%2Fcallback',
-    //   },
-    //   headers: {
-    //     'Content-Type':'application/x-www-form-urlencoded',
-    //     'Authorization': auth,
-    //     'Accept':'application/json'
-    //   }
-    // }).then( (result) => {
-    //   console.log(result);
-    // }).catch( (error) => {
-    //   console.log(error.response);
-    // });
+    spotify.confirmAuth('token');
   }).catch( (error) => {
     console.log(error);
   });
+}
+
+spotify.confirmAuth = () => {
+  axios.post(
+    'https://accounts.spotify.com/api/token', new URLSearchParams ( {
+      grant_type: 'authorization_code',
+      code: spotify.authCode,
+      redirect_uri: `http://localhost:${spotify.port}/`
+    }).toString(),
+    {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': `Basic ${clientId}:${clientSecret}`
+      },
+    }
+  ).then( (response) => {
+    console.log(response);
+  }).catch( (error) => {
+    // console.log(error);
+  })
+
+
+  // console.log(auth);
+  // var auth = 'Basic ' + new Buffer(clientId + ':' + clientSecret);
+  // axios({
+  //   method: 'POST',
+  //   url: 'https://accounts.spotify.com/api/token',
+  //   dataResponse: 'json',
+  //   params : {
+  //     grant_type: 'client_credentials',
+  //     code: authToken,
+  //   },
+  //   data: {
+      
+      
+  //     // redirect_uri: 'https%3A%2F%2Fmikosramek.ca%2Fcallback',
+  //   },
+  //   headers: {
+  //     'Content-Type':'application/x-www-form-urlencoded',
+  //     'Authorization': auth,
+  //     'Accept':'application/json'
+  //   }
+  // }).then( (result) => {
+  //   console.log(result);
+  // }).catch( (error) => {
+  //   console.log(error.response);
+  // });
 }
 
 exports.s = spotify;
